27303102c96534cea7840ed961eeb839d9c9768f
Dasari Kamali (LTIMINDTREE LIMITED) <v-dkamali@microsoft.com>
2025-02-27 16:57:03 +0530

    first commit

---
+++ src/main/java/com/example/SpringGitHub/SpringGitHubApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringGitHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGitHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGitHubApplication.class, args);
+	}
+
+}
